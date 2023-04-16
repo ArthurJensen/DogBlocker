c4bf58efe933c9f3cdaa2422a7af5043a4e164b7
Arthur <91498143+ArthurJensen@users.noreply.github.com>
2023-04-15 17:21:35 -0700

    Delete content.js

--- content.js
+++
@@ -1,5 +0,0 @@
-if (window.location.href.startsWith("chrome-extension://")) {
-  var dogImage = new Image();
-  dogImage.src = chrome.extension.getURL("dog.jpg");
-  document.body.style.backgroundImage = `url(${dogImage.src})`;
-}
